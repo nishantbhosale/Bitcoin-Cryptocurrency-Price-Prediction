f6a37092f6a52d4f996dac93a0704fdbf12f6bda
nbhosale <bhosale.nishant.n@gmail.com>
2022-03-06 00:02:00 +0530

    added database and established the connection...

--- app.py
+++ app.py
@@ -1,26 +1,78 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template,request,flash,redirect,url_for,session
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask("Cryptocurrency")
-login_dict = {"nishant@gmail.com":"12345"}
+app.secret_key="123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+print(db)
+con=sqlite3.connect("data.db")
+con.execute("create table if not exists person(id integer primary key,name text,email text,password text)")
+con.close()
+print(con)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
-    r_email = request.args.get("email_r")
-    r_password = request.args.get("password_r")
-    new_dict = {r_email:r_password}
-    login_dict.update(new_dict)
+    if request.method=='POST':
+        #name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        con=sqlite3.connect("data.db")
+        con.row_factory=sqlite3.Row
+        cur=con.cursor()
+        cur.execute("select * from person where email=? and password=?",(email,password))
+        data=cur.fetchone()
+
+        if data:
+            session["email"]=data["email"]
+            session["password"]=data["password"]
+            return redirect("prediction")
+        else:
+            return("Username and Password Mismatch")
+            flash("Username and Password Mismatch","danger")
+    #return redirect(url_for("login"))
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register",methods=["GET","POST"])
 def register():
+    if request.method=='POST':
+        try:
+            name=request.form['name']
+            email=request.form['email']
+            password=request.form['password']
+            #db=sqlite3.connect("data.db")
+            #db.session.add(name, email, password)
+            #mail=request.form['mail']
+            con=sqlite3.connect("data.db")
+            cur=con.cursor()
+            cur.execute("insert into person(name,email,password)values(?,?,?);",(name,email,password))
+            con.commit()
+            print("Record Added  Successfully")
+            flash("Record Added  Successfully","success")
+        except:
+            flash("Error in Insert Operation","danger")
+        finally:
+            return redirect(url_for("login"))
+            con.close()
+
     return render_template("registration.html")
 
-@app.route("/prediction")
+@app.route("/prediction",methods=["GET","POST"])
 def prediction():
-    return render_template("index.html")
+    return("hi this is Prediction page")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
 
 if __name__ == '__main__' :
+    db.create_all()
     app.run(debug=True, host='0.0.0.0', port='80')
